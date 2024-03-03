8c32b576c4ac4096825b568717ee7be5ce455c74
williamdual <williamdual115@gmail.com>
2024-03-03 02:00:06 -0500

    changed detection method from masking to color catching

--- main.py
+++ main.py
@@ -1,40 +1,50 @@
 import cv2 as cv
-import argparse
+from cv2.typing import MatLike
 import os
+from gameStrategy import tts
 
-#MOG subtraction stuff
-parser = argparse.ArgumentParser(description='This program shows how to use background subtraction methods provided by \
-                                              OpenCV. You can process both videos and images.')
-parser.add_argument('--input', type=str, help='Path to a video or a sequence of image.', default='vtest.avi')
-parser.add_argument('--algo', type=str, help='Background subtraction method (KNN, MOG2).', default='MOG2')
-args = parser.parse_args()
+#tts
+tts = tts.TTS()
 
 # set up video capture
-capture = cv.VideoCapture(0)
+capture = cv.VideoCapture(0) #Might need to change to 1
 WIDTH, HEIGHT = 1280, 1024
 capture.set(3,  WIDTH)
 capture.set(4, HEIGHT)
 
-if args.algo == 'MOG2':
-    backSub = cv.createBackgroundSubtractorMOG2()
-else:
-    backSub = cv.createBackgroundSubtractorKNN()
-
 if not os.path.exists('images'):
     os.makedirs('images')
 
+
+def contains_card(frame: MatLike) -> bool:
+    WHITE_BOUND = 120
+    width = frame.shape[1] #320
+    height = frame.shape[0] #341
+    matchesRequired = 25000 #max pixel count is around 109,000
+    pixelMatches = 0
+
+    for y in range(0, width):
+        for x in range(0, height):
+            (blue, green, red) = frame[x, y]
+            # print(f"RGB: {red}, {green}, {blue}")
+
+            if blue >= WHITE_BOUND and green >= WHITE_BOUND and red >= WHITE_BOUND:
+                pixelMatches += 1
+            # works = True
+            # for k in color:
+            #     if not (whiteRange[0] > k and k < whiteRange[1]):
+            #         works = False
+            # if works:
+            #     #print("pixel matches ", pixelMatches)
+            #     pixelMatches += 1
+            if pixelMatches >= matchesRequired:
+                return True
+    return False
+
+
+
 while (True):
     _, frame = capture.read()
-
-    cv.line(frame, (0, int(HEIGHT/3)), (WIDTH, int(HEIGHT/3)), (0, 255, 0), 2)
-    for i in range(1, 4):
-        cv.line(frame, (int(WIDTH*i/4), 0), (int(WIDTH*i/4), HEIGHT), (0, 255, 0), 2)
-
-    cv.imshow('stream', frame)
-
-    fgMask = backSub.apply(frame, learningRate=0)
-
-    cv.imshow('FG Mask', fgMask)
 
     if cv.waitKey(5) == ord('b'):
         cv.imwrite('./images/bg.png', frame)
@@ -47,7 +57,23 @@ while (True):
                 partitions.append(frame[int(HEIGHT/3)*i:int(HEIGHT/3)*(i+1), int(WIDTH/4)*j:int(WIDTH/4)*(j+1)])    
 
         for i in range(0, 8):
+            if contains_card(partitions[i]):
+                yes_str = f"{i}: CONTAINS A CARD!"
+                print(yes_str)
+                tts.speak(yes_str)
+            else:
+                no_str = f"{i}: FUCKING DOESNT"
+                print(no_str)
+                tts.speak(no_str)
             cv.imwrite(f'./images/partition_{i}.png', partitions[i])
+        print()
+
+    # alignment guides
+    cv.line(frame, (0, int(HEIGHT/3)), (WIDTH, int(HEIGHT/3)), (0, 255, 0), 2)
+    for i in range(1, 4):
+        cv.line(frame, (int(WIDTH*i/4), 0), (int(WIDTH*i/4), HEIGHT), (0, 255, 0), 2)
+
+    cv.imshow('stream', frame)
 
     if cv.waitKey(5) == ord('q'):
         break
